a161954b5455753a4319480a2d142c1d3039317f
Julien Smadja <julien.smadja@gmail.com>
2014-02-03 22:33:48 +0100

    wip

--- app/stx/stx.js
+++ app/stx/stx.js
@@ -1,19 +1,24 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(1200, 600, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update: update });
 
 function preload() {
-
     game.load.image('bullet', 'assets/games/invaders/bullet.png');
     game.load.image('enemyBullet', 'assets/games/invaders/enemy-bullet.png');
     game.load.spritesheet('invader', 'assets/games/invaders/invader32x32x4.png', 32, 32);
+    game.load.image('boss', 'stx_assets/sprites/boss.png', 193, 94);
+    game.load.spritesheet('cto1', 'assets/games/tanks/tank2.png', 32, 32);
+    game.load.spritesheet('cto2', 'assets/games/tanks/tank3.png', 32, 32);
+    game.load.spritesheet('coo', 'assets/games/tanks/tank4.png', 32, 32);
+    game.load.spritesheet('ceo', 'assets/games/tanks/tank5.png', 32, 32);
     game.load.image('ship', 'assets/games/invaders/player.png');
     game.load.spritesheet('kaboom', 'assets/games/invaders/explode.png', 128, 128);
     game.load.image('starfield', 'assets/games/invaders/starfield.png');
     game.load.image('background', 'assets/games/starstruck/background2.png');
-
+    game.load.image('boss_face', 'stx_assets/sprites/boss_face.png', 200, 266);
 }
 
+var MAX_LIVES = 1;
 var player;
-var aliens;
+var aliens, boss, cto, coo, ceo;
 var bullets;
 var bulletTime = 0;
 var cursors;
@@ -28,11 +33,23 @@ var enemyBullet;
 var firingTimer = 0;
 var stateText;
 
+var TITLE_STEP = -1;
+var STAGE_STEP = 0;
+var FUNDATION_BOSS_STEP = 1;
+var CTO_STEP = 2;
+var COO_STEP = 3;
+var CEO_STEP = 4;
+var CONGRATULATION_STEP = 5;
+var HALL_OF_FAME_STEP = 6;
+
+var currentStep;
+
+var hallOfFameChrono, titleChrono;
+
 function create() {
 
     //  The scrolling starfield background
-    starfield = game.add.tileSprite(0, 0, 800, 600, 'starfield');
-
+    starfield = game.add.tileSprite(200, 0, 800, 600, 'starfield');
 
     //  Our bullet group
     bullets = game.add.group();
@@ -54,24 +71,28 @@ function create() {
 
     //  The baddies!
     aliens = game.add.group();
+    boss = game.add.group();
+    cto = game.add.group();
+    coo = game.add.group();
+    ceo = game.add.group();
 
-    createAliens();
+    //
 
     //  The score
     scoreString = 'Score : ';
-    scoreText = game.add.text(10, 10, scoreString + score, { fontSize: '34px', fill: '#fff' });
+    scoreText = game.add.text(10, 100, scoreString + score, { fontSize: '34px', fill: '#fff' });
 
     //  Lives
     lives = game.add.group();
-    game.add.text(game.world.width - 100, 10, 'Lives : ', { fontSize: '34px', fill: '#fff' });
+    game.add.text(0, 10, 'Lives : ', { fontSize: '34px', fill: '#fff' });
 
     //  Text
     stateText = game.add.text(game.world.centerX, game.world.centerY, '', { fontSize: '84px', fill: '#fff' });
     stateText.anchor.setTo(0.5, 0.5);
     stateText.visible = false;
 
-    for (var i = 0; i < 3; i++) {
-        var ship = lives.create(game.world.width - 100 + (30 * i), 60, 'ship');
+    for (var i = 0; i < MAX_LIVES; i++) {
+        var ship = lives.create(0 + (30 * i), 60, 'ship');
         ship.anchor.setTo(0.5, 0.5);
         ship.angle = 90;
         ship.alpha = 0.4;
@@ -85,41 +106,9 @@ function create() {
     //  And some controls to play the game with
     cursors = game.input.keyboard.createCursorKeys();
     fireButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-}
 
-function createAliens() {
-
-    for (var y = 0; y < 4; y++) {
-        for (var x = 0; x < 10; x++) {
-            var alien = aliens.create(x * 48, y * 50, 'invader');
-            alien.anchor.setTo(0.5, 0.5);
-            alien.animations.add('fly', [ 0, 1, 2, 3 ], 20, true);
-            alien.play('fly');
-        }
-    }
-
-    aliens.x = 100;
-    aliens.y = 50;
-
-    //  All this does is basically start the invaders moving. Notice we're moving the Group they belong to, rather than the invaders directly.
-    var tween = game.add.tween(aliens).to({ x: 200 }, 2000, Phaser.Easing.Linear.None, true, 0, 1000, true);
-
-    //  When the tween completes it calls descend, before looping again
-    tween.onComplete.add(descend, this);
-}
-
-function setupInvader(invader) {
-
-    invader.anchor.x = 0.5;
-    invader.anchor.y = 0.5;
-    invader.animations.add('kaboom');
-
-}
-
-function descend() {
-
-    aliens.y += 10;
-
+    currentStep = TITLE_STEP;
+    titleChrono = game.time.now;
 }
 
 function update() {
@@ -146,17 +135,143 @@ function update() {
         enemyFires();
     }
 
-    //  Run collision
-    game.physics.collide(bullets, aliens, collisionHandler, null, this);
-    game.physics.collide(enemyBullets, player, enemyHitsPlayer, null, this);
+    if (currentStep === TITLE_STEP) {
+        if (game.time.now > (titleChrono + 10000)) {
+            drawHallOfFame();
+        }
+        game.input.onTap.addOnce(createMissionSelectionStep, this);
+    }
+    if (currentStep === HALL_OF_FAME_STEP) {
+        if (game.time.now > (hallOfFameChrono + 10000)) {
+            drawTitle();
+        }
+        game.input.onTap.addOnce(createMissionSelectionStep, this);
+    }
+    if (currentStep === STAGE_STEP) {
+        game.physics.collide(bullets, aliens, collisionHandler, null, this);
+    }
+    if (currentStep === FUNDATION_BOSS_STEP) {
+        game.physics.collide(bullets, boss, bossCollisionHandler, null, this);
+    }
+    if (currentStep === CTO_STEP) {
+        game.physics.collide(bullets, cto, ctoCollisionHandler, null, this);
+    }
+    if (currentStep === COO_STEP) {
+        game.physics.collide(bullets, coo, cooCollisionHandler, null, this);
+    }
+    if (currentStep === CEO_STEP) {
+        game.physics.collide(bullets, ceo, ceoCollisionHandler, null, this);
+    }
+    if (currentStep === CONGRATULATION_STEP) {
+    }
+    //game.physics.collide(enemyBullets, player, enemyHitsPlayer, null, this);
+}
+
+function createXebians() {
+    var alien = aliens.create(48, 50, 'invader');
+    alien.anchor.setTo(0.5, 0.5);
+    alien.animations.add('fly', [ 0, 1, 2, 3 ], 20, true);
+    alien.play('fly');
+
+
+    aliens.x = 100;
+    aliens.y = 50;
+    enemyBullets.setAll('outOfBoundsKill', true);
+    currentStep = STAGE_STEP;
+
+    //  All this does is basically start the invaders moving. Notice we're moving the Group they belong to, rather than the invaders directly.
+    //var tween = game.add.tween(aliens).to({ x: 200 }, 2000, Phaser.Easing.Linear.None, true, 0, 1000, true);
+
+    //  When the tween completes it calls descend, before looping again
+    //tween.onComplete.add(descend, this);
+}
+
+function drawTitle() {
+    titleChrono = game.time.now;
+    currentStep = TITLE_STEP;
+}
+
+
+function createMissionSelectionStep() {
+    // choose your mission
+    //showStage();
+    createXebians();
+}
+
+function createFundationBoss() {
+    var bossSprite = boss.create(0, 0, 'boss');
+    bossSprite.scale.x = 1.5;
+    bossSprite.scale.y = 1.5;
+    bossSprite.x = 600;
+
+    game.add.sprite(game.world.width - 200, 0, 'boss_face');
+
 
 }
 
-function collisionHandler(bullet, alien) {
+function createCTOBosses() {
+    var cto1 = cto.create(48, 50, 'cto1');
+    cto1.anchor.setTo(0.5, 0.5);
 
-    //  When a bullet hits an alien we kill them both
+    var cto2 = cto.create(48, 50, 'cto2');
+    cto2.anchor.setTo(1, 1);
+
+    cto.x = 100;
+    cto.y = 100;
+}
+
+function createCOO() {
+    var alien = coo.create(48, 50, 'coo');
+    alien.anchor.setTo(0.5, 0.5);
+    coo.x = 100;
+    coo.y = 100;
+}
+
+function createCEO() {
+    var alien = ceo.create(48, 50, 'ceo');
+    alien.anchor.setTo(0.5, 0.5);
+    ceo.x = 100;
+    ceo.y = 100;
+}
+
+function drawHallOfFame() {
+    currentStep = HALL_OF_FAME_STEP;
+    scoreText.visible = false;
+    hallOfFameChrono = game.time.now;
+}
+
+function createCongratulationStep() {
+    scoreText.content = "CONGRATULATIONS";
+    stateText.content = " You Won, \n Click to restart";
+    stateText.visible = true;
+    game.input.keyboard.onDownCallback = inputName;
+}
+
+function inputName(e) {
+    scoreText.content += e.keyCode;
+    if (e.keyCode == '13' || e.keyCode == 13) {
+        drawHallOfFame();
+    }
+}
+
+function setupInvader(invader) {
+    invader.anchor.x = 0.5;
+    invader.anchor.y = 0.5;
+    invader.animations.add('kaboom');
+}
+
+function descend() {
+
+    //aliens.y += 10;
+
+}
+
+
+function collisionHandler(bullet, target) {
+
+    //  When a bullet hits an target we kill them both
     bullet.kill();
-    alien.kill();
+    target.kill();
 
     //  Increase the score
     score += 20;
@@ -164,23 +279,99 @@ function collisionHandler(bullet, alien) {
 
     //  And create an explosion :)
     var explosion = explosions.getFirstDead();
-    explosion.reset(alien.body.x, alien.body.y);
+    explosion.reset(target.body.x, target.body.y);
     explosion.play('kaboom', 30, false, true);
 
     if (aliens.countLiving() == 0) {
-        score += 1000;
-        scoreText.content = scoreString + score;
+        /*
+         score += 1000;
+         scoreText.content = scoreString + score;
 
-        enemyBullets.callAll('kill', this);
-        stateText.content = " You Won, \n Click to restart";
-        stateText.visible = true;
-
-        //the "click to restart" handler
-        game.input.onTap.addOnce(restart, this);
+         enemyBullets.callAll('kill', this);
+         */
+        currentStep = FUNDATION_BOSS_STEP;
+        createFundationBoss();
     }
 
 }
 
+function bossCollisionHandler(bullet, target) {
+
+    //  When a bullet hits an target we kill them both
+    bullet.kill();
+    target.kill();
+
+    //  Increase the score
+    score += 20;
+    scoreText.content = scoreString + score;
+
+    //  And create an explosion :)
+    var explosion = explosions.getFirstDead();
+    explosion.reset(target.body.x, target.body.y);
+    explosion.play('kaboom', 30, false, true);
+
+    if (boss.countLiving() == 0) {
+        currentStep = CTO_STEP;
+        createCTOBosses();
+    }
+
+}
+
+function ctoCollisionHandler(bullet, target) {
+    //  When a bullet hits an target we kill them both
+    bullet.kill();
+    target.kill();
+    //  And create an explosion :)
+    var explosion = explosions.getFirstDead();
+    explosion.reset(target.body.x, target.body.y);
+    explosion.play('kaboom', 30, false, true);
+    if (cto.countLiving() == 0) {
+        currentStep = COO_STEP;
+        createCOO();
+    }
+}
+
+function cooCollisionHandler(bullet, target) {
+    //  When a bullet hits an target we kill them both
+    bullet.kill();
+    target.kill();
+    //  And create an explosion :)
+    var explosion = explosions.getFirstDead();
+    explosion.reset(target.body.x, target.body.y);
+    explosion.play('kaboom', 30, false, true);
+    if (coo.countLiving() == 0) {
+        currentStep = CEO_STEP;
+        createCEO();
+    }
+}
+
+function ceoCollisionHandler(bullet, target) {
+    //  When a bullet hits an target we kill them both
+    bullet.kill();
+    target.kill();
+    //  And create an explosion :)
+    var explosion = explosions.getFirstDead();
+    explosion.reset(target.body.x, target.body.y);
+    explosion.play('kaboom', 30, false, true);
+    if (ceo.countLiving() == 0) {
+        currentStep = CONGRATULATION_STEP;
+        createCongratulationStep();
+    }
+
+}
+
+
+function gameOver() {
+    player.kill();
+    enemyBullets.callAll('kill');
+
+    stateText.content = " GAME OVER \n Click to restart";
+    stateText.visible = true;
+
+    //the "click to restart" handler
+    game.input.onTap.addOnce(restart, this);
+
+}
 function enemyHitsPlayer(player, bullet) {
 
     bullet.kill();
@@ -198,14 +389,7 @@ function enemyHitsPlayer(player, bullet) {
 
     // When the player dies
     if (lives.countLiving() < 1) {
-        player.kill();
-        enemyBullets.callAll('kill');
-
-        stateText.content = " GAME OVER \n Click to restart";
-        stateText.visible = true;
-
-        //the "click to restart" handler
-        game.input.onTap.addOnce(restart, this);
+        gameOver();
     }
 
 }
@@ -256,26 +440,21 @@ function fireBullet() {
 
 }
 
-function resetBullet(bullet) {
-
-    //  Called if the bullet goes out of the screen
-    bullet.kill();
-
-}
-
 function restart() {
+    /*
+     //  A new level starts
 
-    //  A new level starts
+     //resets the life count
+     lives.callAll('revive');
+     //  And brings the aliens back from the dead :)
+     aliens.removeAll();
+     createXebians();
 
-    //resets the life count
-    lives.callAll('revive');
-    //  And brings the aliens back from the dead :)
-    aliens.removeAll();
-    createAliens();
+     //revives the player
+     player.revive();
+     //hides the text
+     stateText.visible = false;
 
-    //revives the player
-    player.revive();
-    //hides the text
-    stateText.visible = false;
-
+     currentStep = STAGE_STEP;
+     */
 }
